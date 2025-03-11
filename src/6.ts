137d8d3d57a7dc37ebd9cfbe5655df5a917b4850
overlandyutzy261 <overlandyutzy261@gmail.com>
2025-03-11 03:16:27 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,8 @@
+// TypeScript code for school project
+
+class School {
+  constructor(public name: string) {}
+}
+
+let mySchool = new School("ABC High School");
+console.log(mySchool.name); // Output: "ABC High School"
